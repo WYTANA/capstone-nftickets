ee4bd566353145460a61a345d96fb25ccddcb85b
Travis L Orback <tlorback@gmail.com>
2022-04-08 17:52:18 -0500

    UPDATE: misc

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
         <img
           className="ticketIcon"
           src="https://th.bing.com/th/id/R.376d5eb97e12141142e1b2eceeac7c5b?rik=gOOTjBC7wsgACQ&riu=http%3a%2f%2fclipart-library.com%2fimages_k%2fticket-clipart-transparent%2fticket-clipart-transparent-9.png&ehk=2acrXCfUVQO76QJMLvl64%2fJtgNmSrXR6AGSPZVmJcic%3d&risl=&pid=ImgRaw&r=0"
-          alt=""
+          alt="ticket"
         ></img>
         <h1>NFTickets</h1>
       </div>
